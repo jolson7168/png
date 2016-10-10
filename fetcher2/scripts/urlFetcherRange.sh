313d90f9816c88a136bf938965ab36f46dfaea94
Test <test@test.com>
2016-10-10 12:37:35 +0000

    updated

--- fetcher2/scripts/urlFetcherRange.sh
+++ fetcher2/scripts/urlFetcherRange.sh
@@ -1,1 +1,5 @@
-python ../src/urlFetcher.py -c ../config/urlFetcher.conf --startDate 2016/08/28/00 --endDate 2016/08/28/23
+#!/bin/bash
+
+echo `date +"%Y-%m-%d %H:%M:%S"` Starting urlFetcher job.... >../logs/`date +"%Y%m%d%H%M%S"`_urlFetcher.log 
+python ../src/urlFetcher.py -c ../config/urlFetcher.conf --startDate 2016/10/09/00 --endDate 2016/10/09/23
+echo `date +"%Y-%m-%d %H:%M:%S"` urlFetcher job complete! >>../logs/`date +"%Y%m%d%H%M%S"`_urlFetcher.log
